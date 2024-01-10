bbd7fe239131f2e61fea3bde0b635137b9f4c417
Diyorka <diyorp2@mail.ru>
2024-01-10 23:26:32 +0600

    Added cross origin

--- src/main/java/kg/nurtelecom/opinion/controller/HelloController.java
+++ src/main/java/kg/nurtelecom/opinion/controller/HelloController.java
@@ -1,12 +1,14 @@
 package kg.nurtelecom.opinion.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class HelloController {
 
     @GetMapping("/hello")
