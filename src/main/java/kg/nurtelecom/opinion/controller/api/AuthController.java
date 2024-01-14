919c0755ecb78d9cc10fdf8fce08b86bb4e0d79b
Diyorka <diyorp2@mail.ru>
2024-01-14 23:05:46 +0600

    Fixed sign in path

--- src/main/java/kg/nurtelecom/opinion/controller/api/AuthController.java
+++ src/main/java/kg/nurtelecom/opinion/controller/api/AuthController.java
@@ -35,7 +35,7 @@ public class AuthController {
         return authService.signUp(user);
     }
 
-    @PostMapping("/login")
+    @PostMapping("/sign-in")
     @Operation(
             summary = "Вход в аккаунт"
     )
