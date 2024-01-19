bbd998ff4a9e45058da552fc1158e64aeca0e679
NursTaal <nursultantaalaibekov01@gmail.com>
2024-01-19 17:38:40 +0300

    done tag entity link with article entity

--- src/main/java/kg/nurtelecom/opinion/entity/Article.java
+++ src/main/java/kg/nurtelecom/opinion/entity/Article.java
@@ -5,6 +5,7 @@ import kg.nurtelecom.opinion.enums.ArticleStatus;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "article")
@@ -27,8 +28,11 @@ public class Article extends BaseEntity {
 
     private Long viewsCount;
 
-
-
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tags_articles",
+            joinColumns = @JoinColumn(name = "article_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tagSet;
 
     public Article() {
     }
