0f894272fa408f307d6d22f5727c60fdac5153ab
invsblmad <aizatmadumarova04@gmail.com>
2024-01-19 15:14:14 +0600

    removed comment status and modified comment deleting logic

--- src/main/java/kg/nurtelecom/opinion/enums/CommentStatus.java
+++
@@ -1,5 +0,0 @@
-package kg.nurtelecom.opinion.enums;
-
-public enum CommentStatus {
-    ACTIVE, DELETED, ALTERED
-}
--- src/main/java/kg/nurtelecom/opinion/payload/comment/CommentResponse.java
+++ src/main/java/kg/nurtelecom/opinion/payload/comment/CommentResponse.java
@@ -1,7 +1,6 @@
 package kg.nurtelecom.opinion.payload.comment;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import kg.nurtelecom.opinion.enums.CommentStatus;
 import kg.nurtelecom.opinion.payload.user.UserResponse;
 
 import java.time.LocalDateTime;
@@ -11,7 +10,7 @@ public record CommentResponse(
         String text,
         @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
         LocalDateTime date,
-        CommentStatus status,
+        Boolean altered,
         UserResponse user
 ) {
 }
