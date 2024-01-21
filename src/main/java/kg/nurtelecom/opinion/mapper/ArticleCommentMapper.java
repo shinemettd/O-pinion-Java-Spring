4db3df68e3c0e063e52d50b7028268b8795150b8
invsblmad <aizatmadumarova04@gmail.com>
2024-01-21 18:31:06 +0600

    optimized the requests of getting root comments and their replies

--- src/main/java/kg/nurtelecom/opinion/mapper/ArticleCommentMapper.java
+++ src/main/java/kg/nurtelecom/opinion/mapper/ArticleCommentMapper.java
@@ -3,13 +3,10 @@ package kg.nurtelecom.opinion.mapper;
 import kg.nurtelecom.opinion.entity.ArticleComment;
 import kg.nurtelecom.opinion.payload.comment.CommentRequest;
 import kg.nurtelecom.opinion.payload.comment.CommentResponse;
-import kg.nurtelecom.opinion.payload.comment.NestedCommentResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ArticleCommentMapper {
     ArticleComment toEntity(CommentRequest commentRequest);
     CommentResponse toModel(ArticleComment articleComment);
-    NestedCommentResponse toNestedModel(ArticleComment articleComment);
 }
--- src/main/java/kg/nurtelecom/opinion/service/CommentService.java
+++ src/main/java/kg/nurtelecom/opinion/service/CommentService.java
@@ -1,15 +1,17 @@
 package kg.nurtelecom.opinion.service;
 
 import kg.nurtelecom.opinion.entity.User;
+import kg.nurtelecom.opinion.payload.comment.CommentRepliesView;
 import kg.nurtelecom.opinion.payload.comment.CommentRequest;
 import kg.nurtelecom.opinion.payload.comment.CommentResponse;
-import kg.nurtelecom.opinion.payload.comment.NestedCommentResponse;
+import kg.nurtelecom.opinion.payload.comment.CommentView;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 public interface CommentService {
-    ResponseEntity<Page<NestedCommentResponse>> getAllComments(Long articleId, Pageable pageable);
+    ResponseEntity<Page<CommentView>> getRootComments(Long articleId, Pageable pageable);
+    ResponseEntity<Page<CommentRepliesView>> getCommentReplies(Long id, Pageable pageable);
     ResponseEntity<CommentResponse> saveComment(Long articleId, CommentRequest commentRequest, User user);
     ResponseEntity<CommentResponse> replyToComment(Long id, CommentRequest commentRequest, User user);
     ResponseEntity<CommentResponse> updateCommentById(Long id, CommentRequest commentRequest, User user);
