8c85e91ef26f061c7248ea4d1d2fc9bcbce699a0
Diyorka <diyorp2@mail.ru>
2024-01-26 22:32:19 +0600

    Fixed complaint adding

--- src/main/java/kg/nurtelecom/opinion/controller/api/ComplaintController.java
+++ src/main/java/kg/nurtelecom/opinion/controller/api/ComplaintController.java
@@ -3,13 +3,15 @@ package kg.nurtelecom.opinion.controller.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import kg.nurtelecom.opinion.entity.User;
 import kg.nurtelecom.opinion.payload.complaint.ComplaintDTO;
 import kg.nurtelecom.opinion.service.ComplaintService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/complaints/on-article")
+@RequestMapping("/api/complaints")
 @Tag(
         name = "Контроллер для создания жалоб",
         description = "В этом контроллере описаны на создание жалоб"
@@ -22,13 +24,13 @@ public class ComplaintController {
         this.complaintService = complaintService;
     }
 
-    @PostMapping("/{id}")
+    @PostMapping("/{article-id}")
     @Operation(
-            summary = "Создание статьи"
+            summary = "Добавление жалобы"
     )
-    public ResponseEntity<ComplaintDTO> createComplain(@PathVariable("id") Long id,
-                                                       @RequestBody @Valid ComplaintDTO complaintDTO){
-        System.out.println(complaintDTO);
-        return complaintService.createComplaint(id, complaintDTO);
+    public ResponseEntity<ComplaintDTO> createComplain(@PathVariable("article-id") Long articleId,
+                                                       @Valid @RequestBody ComplaintDTO complaintDTO,
+                                                       @AuthenticationPrincipal User user){
+        return complaintService.createComplaint(articleId, complaintDTO, user);
     }
 }
