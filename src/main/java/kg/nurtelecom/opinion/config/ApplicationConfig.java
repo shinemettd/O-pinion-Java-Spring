92e4a1249165dc6ac68a563ffecd2059990910d6
kimgalina <genrietta.kim@gmail.com>
2024-01-31 17:50:06 +0600

    Added functionality for uploading images to cloudinary

--- src/main/java/kg/nurtelecom/opinion/config/ApplicationConfig.java
+++ src/main/java/kg/nurtelecom/opinion/config/ApplicationConfig.java
@@ -1,7 +1,10 @@
 package kg.nurtelecom.opinion.config;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import kg.nurtelecom.opinion.exception.NotFoundException;
 import kg.nurtelecom.opinion.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,6 +24,16 @@ import java.util.List;
 public class ApplicationConfig {
     private final UserRepository userRepository;
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloud_name;
+
+    @Value("${cloudinary.api_key}")
+    private String api_key;
+
+    @Value("${cloudinary.api_secret}")
+    private String api_secret;
+
+
     public ApplicationConfig(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -29,6 +42,14 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return email -> userRepository.findByEmail(email)
                 .orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloud_name,
+                "api_key", api_key,
+                "api_secret", api_secret));
     }
 
     @Bean
