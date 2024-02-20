7005d4152ecc7e0a8cf506e60d5e910dd9a1874d
azidinkun <azidinkun@icloud.com>
2024-02-20 15:07:33 +0600

    create the class PasswordResetTokenRepositoryTest

---
+++ src/test/java/kg/nurtelecom/opinion/repository/PasswordResetTokenRepositoryTest.java
@@ -0,0 +1,9 @@
+package kg.nurtelecom.opinion.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PasswordResetTokenRepositoryTest {
+}
